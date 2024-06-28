17d3b384d75dc604006f65010922a8483a255d90
Shiv <devendraspatil1995@gmail.com>
2024-06-28 15:21:44 +0530

    PUT-update user in UserControllClass

--- blog-app-apis/src/main/java/com/bikkadit/blog/controller/UserController.java
+++ blog-app-apis/src/main/java/com/bikkadit/blog/controller/UserController.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,6 +33,12 @@ public class UserController {
 	}
 
 	// PUT-Update user
+	@PutMapping("/user/{userId}") // {userId} this is path uri variable
+	public ResponseEntity<UserDto> updateUser(@RequestBody UserDto userDto, @PathVariable("userId") Integer uid) {
+		UserDto updatedUser = this.userService.updatUser(userDto, uid);
+		return ResponseEntity.ok(updatedUser);
+
+	}
 
 	// DELETE-delete user
 
