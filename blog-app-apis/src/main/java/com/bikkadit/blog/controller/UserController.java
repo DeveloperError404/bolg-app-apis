ef58950216085f1b52249119ae15dace9562c3f8
Shiv <devendraspatil1995@gmail.com>
2024-06-28 15:44:05 +0530

    DELETE- delete user logic in UserController

--- blog-app-apis/src/main/java/com/bikkadit/blog/controller/UserController.java
+++ blog-app-apis/src/main/java/com/bikkadit/blog/controller/UserController.java
@@ -1,8 +1,11 @@
 package com.bikkadit.blog.controller;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.bikkadit.blog.payloads.ApiResponse;
 import com.bikkadit.blog.payloads.UserDto;
 import com.bikkadit.blog.services.UserService;
 
@@ -41,6 +45,11 @@ public class UserController {
 	}
 
 	// DELETE-delete user
+	@DeleteMapping("/user/{userId}")
+	public ResponseEntity<ApiResponse> deleteUser(@PathVariable("userId") Integer uid) {
+		this.deleteUser(uid);
+		return new ResponseEntity<ApiResponse>(new ApiResponse("User Deleted Successfully", true), HttpStatus.OK);
+	}
 
 	// GET-get user
 
